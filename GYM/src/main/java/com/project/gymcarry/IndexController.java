c59cedbcc524825010528d87ad672b57f17b1881
ricky9397 <kimzzan1234@gmail.com>
2021-08-20 18:59:34 +0900

    commit
    commit

--- GYM/src/main/java/com/project/gymcarry/IndexController.java
+++ GYM/src/main/java/com/project/gymcarry/IndexController.java
@@ -1,19 +1,8 @@
 package com.project.gymcarry;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class IndexController {
 
@@ -26,5 +15,7 @@ public class IndexController {
 		System.out.println("/ 진입");
 		return "redirect:/index";
 	}
+
+
 
 }

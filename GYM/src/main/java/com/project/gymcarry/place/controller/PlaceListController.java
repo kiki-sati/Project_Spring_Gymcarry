53a208badaaf367f84fe6b5953ace1172ae4fbc4
seungmin1592 <tmdals1592@naver.com>
2021-09-03 16:54:05 +0900

    Update PlaceListController.java

--- GYM/src/main/java/com/project/gymcarry/place/controller/PlaceListController.java
+++ GYM/src/main/java/com/project/gymcarry/place/controller/PlaceListController.java
@@ -29,20 +29,20 @@ public class PlaceListController {
 	private PlaceHealthService placeHealthService;
 	
 	// place 리스트
-	@GetMapping
-	public String placeList(
-				@RequestParam("placenum") int placenum,
-				Model model
-			) {
-		List<PlaceDto> placeList = placeHealthService.getHealthPlaceList(placenum);
-		System.out.println("placenum : " + placenum);
-		
-		model.addAttribute("placeList", placeList);
-		
-		
-		return "place/placeList";
-	}
-	
+//	@GetMapping
+//	public String placeList(
+//				@RequestParam("placenum") int placenum,
+//				Model model
+//			) {
+//		List<PlaceDto> placeList = placeHealthService.getHealthPlaceList(placenum);
+//		System.out.println("placenum : " + placenum);
+//		
+//		model.addAttribute("placeList", placeList);
+//		
+//		
+//		return "place/placeList";
+//	}
+//	
 	// 컨트롤러 부분
 //	@RequestMapping(value = "/json", method = RequestMethod.GET, produces="text/plain;charset=UTF-8")
 //	@ResponseBody
@@ -57,21 +57,21 @@ public class PlaceListController {
 	
 	
 	
-	@RequestMapping(value="/search", method=RequestMethod.GET)
-    public void search(String placename, HttpServletResponse response) throws Exception {
-        List<PlaceSearchDto> list = placeHealthService.getPlaceSearch();
-        // 응답해야 하는 문자열 : [{label:~,value:~},{label:~,value:~}]
-
-        JSONArray array = new JSONArray();
-        for(PlaceSearchDto dto : list) {
-            JSONObject obj = new JSONObject();
-            obj.put("label", dto.getPlacename());
-            obj.put("value", dto.getPlacename());
-
-            array.add(obj);
-        }
-
-	}
+//	@RequestMapping(value="/search", method=RequestMethod.GET)
+//    public void search(String placename, HttpServletResponse response) throws Exception {
+//        List<PlaceSearchDto> list = placeHealthService.getPlaceSearch();
+//        // 응답해야 하는 문자열 : [{label:~,value:~},{label:~,value:~}]
+//
+//        JSONArray array = new JSONArray();
+//        for(PlaceSearchDto dto : list) {
+//            JSONObject obj = new JSONObject();
+//            obj.put("label", dto.getPlacename());
+//            obj.put("value", dto.getPlacename());
+//
+//            array.add(obj);
+//        }
+//
+//	}
 
 
 

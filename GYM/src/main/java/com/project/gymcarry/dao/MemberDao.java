7ff9047ce82a10e62d37651789755cedf4eb12f0
EunKyung Shin <heybrilliante@gmail.com>
2021-09-07 23:10:46 +0900

    은경_회원가입 이메일 인증

--- GYM/src/main/java/com/project/gymcarry/dao/MemberDao.java
+++ GYM/src/main/java/com/project/gymcarry/dao/MemberDao.java
@@ -1,4 +1,3 @@
-  
 package com.project.gymcarry.dao;
 
 import com.project.gymcarry.carry.CarryJoinDto;
@@ -11,8 +10,18 @@ public interface MemberDao {
 	SessionDto memberLogin(String id, String pw);
 	// 멤버 회원가입
 	int insertMember(MemberDto memberDto);
-	// 케리회원가입
+	// 캐리회원가입
 	int insertCarry(CarryJoinDto carryDto);
-	// 케리로그인
+	// 캐리로그인
 	SessionDto carryLogin(String id, String pw);
+	// 회원가입시 인증키 생성
+	
+	// 멤버 회원가입시 이메일 중복 체크
+	
+	// 멤버 회원가입 이메일 인증
+	int GetJoinkey(String mememail, String joinkey_status);
+	// 인증키 변경 
+	int alterjoinkey(String mememail, String joinkey);
+	// 로그인시 joinkey_status 확인
+	MemberDto Memberjoinkeycheck(String id, String pw, String joinkey_status);
 }

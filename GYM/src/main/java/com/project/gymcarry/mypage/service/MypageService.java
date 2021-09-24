d07f2a11ccf1e0e79d8068a73972cc491d87cc2d
김훈 <50814859+alghrksl@users.noreply.github.com>
2021-09-24 10:28:48 +0900

    Update mp error

--- GYM/src/main/java/com/project/gymcarry/mypage/service/MypageService.java
+++ GYM/src/main/java/com/project/gymcarry/mypage/service/MypageService.java
@@ -9,11 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.gymcarry.carry.CarryMypageDto;
-import com.project.gymcarry.dao.CarryMyPageDao;
 import com.project.gymcarry.dao.MypageDao;
-import com.project.gymcarry.mypage.MypageDto;
 import com.project.gymcarry.mypage.MypageDto2;
-import com.project.gymcarry.mypage.MypagePaymentDto;
 
 @Service
 public class MypageService {
@@ -23,22 +20,22 @@ public class MypageService {
 
 	private MypageDao dao;
 
-	public int memberMemo(MypageDto mypDto) {
+	public int memberMemo(MypageDto2 mypdto) {
 		dao = template.getMapper(MypageDao.class);
-		return dao.insertMemo(mypDto);
+		return dao.insertMemo(mypdto);
 	}
 
-	public int updateMemo(MypageDto mypDto) {
+	public int updateMemo(MypageDto2 mypdto) {
 		dao = template.getMapper(MypageDao.class);
-		return dao.updateMemo(mypDto);
+		return dao.updateMemo(mypdto);
 	}
 
-	public List<MypageDto> selectMemo(String memidx, String infodate, String infotype) {
+	public List<MypageDto2> selectMemo(String memidx, String infodate, String infotype) {
 		dao = template.getMapper(MypageDao.class);
 		return dao.selectMemo(memidx, infodate, infotype);
 	}
 
-	public List<MypageDto> loadMemo(String arg0, String arg1) {
+	public List<MypageDto2> loadMemo(String arg0, String arg1) {
 		dao = template.getMapper(MypageDao.class);
 		return dao.loadMemo(arg0, arg1);
 	}
@@ -47,20 +44,20 @@ public class MypageService {
 		dao = template.getMapper(MypageDao.class);
 		return dao.loadMemo2(i, arg1);
 	}
-	
-	// 캐리마이페이지 달력때문에 은경이 추가 
-	public int carrycalendar(List<CarryMypageDto> request){
+
+	// 캐리마이페이지 달력때문에 은경이 추가
+	public int carrycalendar(List<CarryMypageDto> request) {
 		dao = template.getMapper(MypageDao.class);
 		return dao.carrycalendar(request);
 	}
-		
-	public List<CarryMypageDto> scheduleview(int cridx){
+
+	public List<CarryMypageDto> scheduleview(int cridx) {
 		dao = template.getMapper(MypageDao.class);
 		return dao.scheduleview(cridx);
 	}
-	
+
 	public void deleteschedule(int cridx) {
 		dao = template.getMapper(MypageDao.class);
 	}
-	
+
 }

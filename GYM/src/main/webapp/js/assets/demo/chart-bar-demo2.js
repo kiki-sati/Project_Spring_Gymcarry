223f38caf13aa79e4c0052641df17b0a9036107b
ricky9397 <kimzzan1234@gmail.com>
2021-09-25 15:27:36 +0900

    commit

--- GYM/src/main/webapp/js/assets/demo/chart-bar-demo2.js
+++ GYM/src/main/webapp/js/assets/demo/chart-bar-demo2.js
@@ -27,7 +27,10 @@ function getBaryear(total, year){
 	      		'rgba(255, 159, 64, 0.5)',
 	      		'#FF9DFF',
 	      		'#FF8C8C',
-	      		'#CBFF75'
+	      		'#CBFF75',
+	      		'#FFD700',
+	      		'#0064CD',
+	      		'#9400D3'
 	      ],
 	      borderColor: "rgba(2,117,216,1)",
 	      data: total,
